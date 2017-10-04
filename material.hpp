bdd22c636d6241b0ff9d9c3b1b3a78a029cc169b
bucketzxm <bucketzxm@gmail.com>
2017-10-04 23:09:08 +0800

    dielectrics

--- material.hpp
+++ material.hpp
@@ -28,6 +28,18 @@ bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted){
   return false;
 }
 
+
+float schlick(float cosine, float ref_idx){
+  // real glass has reflectivity that varies with angle -- look at a window at a steep angle
+  // and it becomes a mirror
+
+  float r0 = (1 - ref_idx) / (1 + ref_idx);
+  r0 = r0*r0;
+  return r0 + (1-r0)*pow((1-cosine), 5);
+
+}
+
+
 class material{
 public:
   virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
@@ -63,7 +75,48 @@ public:
 };
 
 class dielectric: public material{
+public:
+  dielectric(float ri): ref_idx(ri) {}
+  virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const{
+    vec3 outward_normal;
+    vec3 reflected = reflect(r_in.direction(), rec.normal);
 
+    float ni_over_nt;
+    attenuation = vec3(1.0, 1.0, 1.0);
+    vec3 refracted;
+
+    float reflect_prob;
+    float cosine;
+
+    if(dot(r_in.direction(), rec.normal) > 0){
+      outward_normal = -rec.normal;
+      ni_over_nt = ref_idx;
+      cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    }
+    else{
+      outward_normal = rec.normal;
+      ni_over_nt = 1.0 / ref_idx;
+      cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    }
+    if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)){
+      // scattered = ray(rec.p, refracted);
+      reflect_prob = schlick(cosine, ref_idx);
+    }
+    else{
+      scattered = ray(rec.p, reflected);
+      reflect_prob = 1.0;
+    }
+
+    if(drand48() < reflect_prob){
+      scattered = ray(rec.p, reflected);
+    }
+    else{
+      scattered = ray(rec.p, refracted);
+    }
+    return true;
+  }
+
+  float ref_idx;
 };
 
 
